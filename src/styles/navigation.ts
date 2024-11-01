2fd52c4da85d241b3e2167822e25ed777188fad5
Paul Rattigan <paulrattigan@Pauls-MacBook-Pro.local>
2024-11-01 13:05:52 +1100

    feat: implement modular style system

---
+++ src/styles/navigation.ts
@@ -0,0 +1,7 @@
+export const navStyles = {
+  link: {
+    base: `inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium`,
+    active: `border-primary-500 text-gray-900`,
+    inactive: `border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700`
+  }
+}; 
